c46f9febab86c852dab57564a1d21bd3562f2497
gabbers81 <gabriel.garrido18@gmail.com>
2016-11-10 23:59:32 -0400

    Optimizing for production

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,31 +1,26 @@
-import { ErrorService } from './shared/error.service';
-import { ErrorComponent } from './shared/error.component';
-import { NavBarComponent } from './shared/navbar.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { HttpModule } from "@angular/http";
 
-
 import { AppComponent } from "./app.component";
-import { AuthenticationComponent } from "./user/authentication.component";
+import { AuthenticationComponent } from "./auth/authentication.component";
 import { HeaderComponent } from "./header.component";
 import { routing } from "./app.routing";
-import { AuthService } from "./user/auth.service";
-import { MessageModule } from "../app/messages/message.module"
+import { AuthService } from "./auth/auth.service";
+import { ErrorComponent } from "./errors/error.component";
+import { ErrorService } from "./errors/error.service";
+import { MessageModule } from "./messages/message.module";
 
 @NgModule({
     declarations: [
         AppComponent,
         AuthenticationComponent,
         HeaderComponent,
-        NavBarComponent,
         ErrorComponent
     ],
     imports: [
         BrowserModule,
         routing,
-        ReactiveFormsModule,
         HttpModule,
         MessageModule
     ],
--- assets/app/messages/message.service.ts
+++ assets/app/messages/message.service.ts
@@ -1,17 +1,17 @@
-import { ErrorService } from './../shared/error.service';
 import { Http, Response, Headers } from "@angular/http";
 import { Injectable, EventEmitter } from "@angular/core";
 import 'rxjs/Rx';
 import { Observable } from "rxjs";
 
 import { Message } from "./message.model";
+import { ErrorService } from "../errors/error.service";
 
 @Injectable()
 export class MessageService {
     private messages: Message[] = [];
     messageIsEdit = new EventEmitter<Message>();
 
-    constructor(private http: Http, private _errorService: ErrorService) {
+    constructor(private http: Http, private errorService: ErrorService) {
     }
 
     addMessage(message: Message) {
@@ -31,9 +31,9 @@ export class MessageService {
                 this.messages.push(message);
                 return message;
             })
-            .catch((error: Response) =>{
-                this._errorService.handleError(error.json());
-                return Observable.throw(error.json())
+            .catch((error: Response) => {
+                this.errorService.handleError(error.json());
+                return Observable.throw(error.json());
             });
     }
 
@@ -53,9 +53,9 @@ export class MessageService {
                 this.messages = transformedMessages;
                 return transformedMessages;
             })
-            .catch((error: Response) =>{
-                this._errorService.handleError(error.json());
-                return Observable.throw(error.json())
+            .catch((error: Response) => {
+                this.errorService.handleError(error.json());
+                return Observable.throw(error.json());
             });
     }
 
@@ -71,9 +71,9 @@ export class MessageService {
             : '';
         return this.http.patch('http://localhost:3000/message/' + message.messageId + token, body, {headers: headers})
             .map((response: Response) => response.json())
-            .catch((error: Response) =>{
-                this._errorService.handleError(error.json());
-                return Observable.throw(error.json())
+            .catch((error: Response) => {
+                this.errorService.handleError(error.json());
+                return Observable.throw(error.json());
             });
     }
 
@@ -84,9 +84,9 @@ export class MessageService {
             : '';
         return this.http.delete('http://localhost:3000/message/' + message.messageId + token)
             .map((response: Response) => response.json())
-            .catch((error: Response) =>{
-                this._errorService.handleError(error.json());
-                return Observable.throw(error.json())
+            .catch((error: Response) => {
+                this.errorService.handleError(error.json());
+                return Observable.throw(error.json());
             });
     }
 }
